c11cd6ce202178474fcdb54fc5908e0c764270dc
Abhilash <abhilash.dindalkop@gmail.com>
2018-11-08 18:25:24 -0800

    mongo credentials static

--- app/utils/MongoConnection.java
+++ app/utils/MongoConnection.java
@@ -27,6 +27,16 @@ public class MongoConnection {
 
 	private volatile static MongoDatabase db = null;
 
+	private static String SERVER_URL = "ds147003.mlab.com";
+
+	private static int PORT = 47003;
+
+	private static String USERNAME = "citizen";
+
+	private static String PASSWORD = "citizen@123";
+
+	private static String DBNAME = "citizen";
+
 	/**
 	 * private constructor singleton class
 	 */
@@ -43,20 +53,18 @@ public class MongoConnection {
 			synchronized (someObj) {
 				try {
 					// Primary Server
-					ServerAddress primaryServer = new ServerAddress(System.getenv("PRIMARY_SERVER"),
-							Integer.parseInt(System.getenv("PORT")));
+					ServerAddress primaryServer = new ServerAddress(SERVER_URL, PORT);
 
 					// Secondary Server
-					ServerAddress secondaryServer = new ServerAddress(System.getenv("SECONDARY_SERVER"),
-							Integer.parseInt(System.getenv("PORT")));
+					ServerAddress secondaryServer = new ServerAddress(SERVER_URL, PORT);
 
 					// MongoDB read/write configuration
 					MongoClientOptions.Builder mongoConf = new MongoClientOptions.Builder();
 					mongoConf.writeConcern(WriteConcern.JOURNAL_SAFE);
 					mongoConf.readPreference(ReadPreference.nearest());
 
-					MongoCredential mongoCred = MongoCredential.createScramSha1Credential(System.getenv("USERNAME"),
-							System.getenv("DBNAME"), System.getenv("PASSWORD").toCharArray());
+					MongoCredential mongoCred = MongoCredential.createScramSha1Credential(USERNAME, DBNAME,
+							PASSWORD.toCharArray());
 
 					// Creating MongoClient
 					MongoClient mongoClient = new MongoClient(Arrays.asList(primaryServer, secondaryServer),
@@ -64,7 +72,7 @@ public class MongoConnection {
 
 					Morphia morphia = new Morphia();
 					// return dataStore;
-					DS = morphia.createDatastore(mongoClient, System.getenv("DBNAME"));
+					DS = morphia.createDatastore(mongoClient, DBNAME);
 				} catch (Exception e) {
 					Logger.info(
 							"===========================Error in Connecting to Mongo db==============================");
@@ -82,27 +90,25 @@ public class MongoConnection {
 	public static synchronized MongoDatabase getConnection() {
 
 		if (db == null) {
-			ServerAddress primaryServer = new ServerAddress(System.getenv("PRIMARY_SERVER"),
-					Integer.parseInt(System.getenv("PORT")));
+			ServerAddress primaryServer = new ServerAddress(SERVER_URL, PORT);
 
 			// Secondary Server
-			ServerAddress secondaryServer = new ServerAddress(System.getenv("SECONDARY_SERVER"),
-					Integer.parseInt(System.getenv("PORT")));
+			ServerAddress secondaryServer = new ServerAddress(SERVER_URL, PORT);
 
 			// MongoDB read/write configuration
 			MongoClientOptions.Builder mongoConf = new MongoClientOptions.Builder();
 			mongoConf.writeConcern(WriteConcern.JOURNAL_SAFE);
 			mongoConf.readPreference(ReadPreference.nearest());
 
-			MongoCredential mongoCred = MongoCredential.createScramSha1Credential(System.getenv("USERNAME"),
-					System.getenv("DBNAME"), System.getenv("PASSWORD").toCharArray());
+			MongoCredential mongoCred = MongoCredential.createScramSha1Credential(USERNAME, DBNAME,
+					PASSWORD.toCharArray());
 
 			// Creating MongoClient
 			@SuppressWarnings("resource")
 			MongoClient mongoClient = new MongoClient(Arrays.asList(primaryServer, secondaryServer),
 					Arrays.asList(mongoCred, mongoCred), mongoConf.build());
 
-			db = mongoClient.getDatabase(System.getenv("DBNAME"));
+			db = mongoClient.getDatabase(DBNAME);
 		}
 		return db;
 	}
